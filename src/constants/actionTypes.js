8cd4a62daf2cec8b740be622c1c5d49cb7c7f70a
Kunj Shah <kunjshah45@gmail.com>
2021-06-04 14:27:07 +0530

    Change in api link

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -3,7 +3,7 @@ var API_LINK, FILE_LINK;
 const testAdminAddr = 'testexpapp.nibodh.com';
 const localhostAdminAddr = 'localhost';
 if (window.location.href.indexOf(testAdminAddr) > -1 || window.location.href.indexOf(localhostAdminAddr) > -1) {
-    API_LINK = 'http://localhost:3000/api/';
+    API_LINK = 'https://nibodh-backend.herokuapp.com/api/';
     // API_LINK = 'https://testexpapp.nibodh.com/api/';
     FILE_LINK = 'https://nibodh-educare-v1.s3.us-east-2.amazonaws.com/';
 } else {
